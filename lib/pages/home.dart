0d23c69e0e98384d940dfdad793c68721036cc34
ZHEBINNG <210886@student.upm.edu.my>
2023-12-28 19:10:42 +0800

    Revert "final commit"
    This reverts commit 3855eb32ac7bb520474bccf243b76767a7ca4c7f.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -128,12 +128,12 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.blueGrey[900],
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center, 
+          crossAxisAlignment: CrossAxisAlignment.center, // Center-align items
           children: [
             const SizedBox(height: 20),
             _buildAcUnitListView(),
             const SizedBox(height: 20),
-            const Center( 
+            const Center( // Center the labels
               child: Text(
                 'Actions',
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
